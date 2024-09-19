d08578c81745df230e425268ed87c1cc37d91495
coldenflo <quntessence1@gmail.com>
2024-09-20 03:17:48 +0900

    Добавлены стили для меню и оптимизация свайпа

--- src/App.js
+++ src/App.js
@@ -1,88 +1,39 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
-import Navbar from './components/Navbar';
 import MenuCategory from './components/MenuCategory';
 import Cart from './components/Cart';
 import Checkout from './components/Checkout';
-import OrderConfirmation from './components/OrderConfirmation';
+import styled from 'styled-components';
 
-const App = () => {
+function App() {
+  const [selectedCategory, setSelectedCategory] = useState('Бургеры');
   const [cartItems, setCartItems] = useState([]);
-  const [activeCategory, setActiveCategory] = useState('burgers');
-  const [showCheckout, setShowCheckout] = useState(false);
-  const [orderConfirmed, setOrderConfirmed] = useState(false);
-
-  const addToCart = (item) => {
-    const existingItem = cartItems.find((cartItem) => cartItem.id === item.id);
-    if (existingItem) {
-      setCartItems(
-        cartItems.map((cartItem) =>
-          cartItem.id === item.id
-            ? { ...cartItem, quantity: cartItem.quantity + 1 }
-            : cartItem
-        )
-      );
-    } else {
-      setCartItems([...cartItems, { ...item, quantity: 1 }]);
-    }
-  };
-
-  const updateCartItem = (id, quantity) => {
-    setCartItems(
-      cartItems.map((item) =>
-        item.id === id ? { ...item, quantity } : item
-      )
-    );
-  };
-
-  const removeFromCart = (id) => {
-    setCartItems(cartItems.filter((item) => item.id !== id));
-  };
-
-  const handleCheckout = () => {
-    setShowCheckout(true);
-  };
-
-  const handleOrderConfirmation = () => {
-    setOrderConfirmed(true);
-    setCartItems([]); // Очищаем корзину после подтверждения заказа
-  };
 
   return (
-    <AppWrapper>
-      <Navbar activeCategory={activeCategory} setActiveCategory={setActiveCategory} />
-      {!showCheckout && !orderConfirmed && (
-        <MenuWrapper>
-          <MenuCategory category={activeCategory} addToCart={addToCart} />
-        </MenuWrapper>
-      )}
-      {showCheckout && !orderConfirmed && (
-        <Checkout cartItems={cartItems} handleOrderConfirmation={handleOrderConfirmation} />
-      )}
-      {orderConfirmed && (
-        <OrderConfirmation />
-      )}
-      <Cart
-        cartItems={cartItems}
-        updateCartItem={updateCartItem}
-        removeFromCart={removeFromCart}
-        handleCheckout={handleCheckout}
+    <AppContainer>
+      <MenuCategory
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
       />
-    </AppWrapper>
+      <MenuItems>
+        {/* Здесь будут компоненты для отображения блюд */}
+      </MenuItems>
+      <Cart cartItems={cartItems} />
+      <Checkout cartItems={cartItems} />
+    </AppContainer>
   );
-};
+}
 
 export default App;
 
-const AppWrapper = styled.div`
-  font-family: 'Montserrat', sans-serif;
-  background-color: #f8f8f8;
-  min-height: 100vh;
+// Стили для основного контейнера
+const AppContainer = styled.div`
+  max-width: 400px;
+  margin: 0 auto;
+  background-color: #fff;
+  border-radius: 10px;
+  overflow: hidden;
 `;
 
-const MenuWrapper = styled.div`
+const MenuItems = styled.div`
   padding: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
 `;
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,14 +1,27 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
-  * {
+  body {
     margin: 0;
     padding: 0;
+    font-family: 'Montserrat', sans-serif;
+    background-color: #f5f5f5;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  * {
     box-sizing: border-box;
   }
-  body {
+
+  button {
+    outline: none;
+    border: none;
+    cursor: pointer;
+  }
+
+  input, textarea {
     font-family: 'Montserrat', sans-serif;
-    background-color: #f7f7f7;
   }
 `;
 
